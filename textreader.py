4ec8a30aed5bc12f13bfc4230fc2b1f062a2f373
Alex <alex_renouard1@naylor.edu>
2021-02-04 11:29:57 -0600

    Initial Homework

---
+++ textreader.py
@@ -0,0 +1,10 @@
+# Write a program that will create a dictionary in which the keys are the individual words found
+#  in the file and the values are the number of times each word appears. For example, if the word
+# “the” appears 128 times, the dictionary would contain an element with 'the' as the key and 128
+# as the value. The program should display the frequency of each word.
+
+reader = open("text.txt")
+
+print(reader.read())
+
+reader.close()
